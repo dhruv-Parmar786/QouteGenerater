43ae357bc8d0060d81832eae4fe96fe2089c44b4
dhruv-Parmar786 <dhruvdhruv6019@gmail.com>
2024-10-23 12:52:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// const apiUrl = "https://api.quotable.io/random";
+
+// async function getQoute(url){
+//     const response = await fetch(url)
+//     let data = response.json;
+//     console.log(data)
+// }
+
+// getQoute(apiUrl)
